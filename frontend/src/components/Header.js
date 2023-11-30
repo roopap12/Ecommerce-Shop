115b2ba91fe7337dac08037e8c1671d6684c0ee5
Andrew Spence <aspence3595@gmail.com>
2023-11-30 15:13:23 +1300

    rendering categories of products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
-// import React, { useState } from "react";
-// import Navbar from "./Navbar";
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -25,6 +26,7 @@ const Header = () => {
                       Get Started
                     </a>
                   </li>
+                    <Link to="/productsgrid">
                   <li>
                     <a
                       href="/#"
@@ -47,95 +49,96 @@ const Header = () => {
                       View Products
                     </a>
                   </li>
+                  </Link>
                 </ul>
-                <div className="clients pt-16">
+                {/* <div className="clients pt-16">
                   <h6 className="mb-6 flex items-center text-xs font-normal text-body-color dark:text-dark-6">
                     Fast shipping, great reviews, and high quality
                     <span className="ml-3 inline-block h-px w-8 bg-body-color"></span>
                   </h6>
 
 
-//                   <div className="flex items-center space-x-4">
-//                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
-//   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
-// </svg>
+                   <div className="flex items-center space-x-4">
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
+   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+ </svg>
 
 
-// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
-//   <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-// </svg>
+ <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
+   <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+ </svg>
 
 
-// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
-//   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
-// </svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-20 h-20">
+   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+ </svg>
 
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className="hidden px-4 lg:block lg:w-1/12"></div>
-//             <div className="w-full px-4 lg:w-6/12">
-//               <div className="lg:ml-auto lg:text-right">
-//                 <div className="relative z-10 inline-block pt-11 lg:pt-0">
-//                   <img
-//                     src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
-//                     alt="hero"
-//                     className="max-w-full lg:ml-auto"
-//                   />
-//                   <span className="absolute -bottom-8 -left-8 z-[-1]">
-//                     <svg
-//                       width="93"
-//                       height="93"
-//                       viewBox="0 0 93 93"
-//                       fill="none"
-//                       xmlns="http://www.w3.org/2000/svg"
-//                     >
-//                       <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
-//                       <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
-//                     </svg>
-//                   </span>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
+                   </div>
+                 </div> */}
+               </div>
+             </div>
+             <div className="hidden px-4 lg:block lg:w-1/12"></div>
+             <div className="w-full px-4 lg:w-6/12">
+               <div className="lg:ml-auto lg:text-right">
+                 <div className="relative z-10 inline-block pt-11 lg:pt-0">
+              <img
+                    src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+                    alt="hero"
+                    className="max-w-full lg:ml-auto"
+                  />
+                  <span className="absolute -bottom-8 -left-8 z-[-1]">
+                    <svg
+                      width="93"
+                      height="93"
+                      viewBox="0 0 93 93"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                      <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                      <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                      <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                      <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                      <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                      <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                      <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                      <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                      <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                      <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                      <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                      <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                      <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                      <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                      <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                      <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                      <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                      <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                      <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                      <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                      <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                      <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                      <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                      <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-// const SingleImage = ({ href, imgSrc }) => {
-//   return (
-//     <>
-//       <a href={href} className="flex w-full items-center justify-center">
-//         <img src={imgSrc} alt="brand image" className="h-10 w-full" />
-//       </a>
-//     </>
-//   );
-// };
+const SingleImage = ({ href, imgSrc }) => {
+  return (
+    <>
+      <a href={href} className="flex w-full items-center justify-center">
+        <img src={imgSrc} alt="brand image" className="h-10 w-full" />
+      </a>
+    </>
+  );
+};
 
-// export default Header;
+export default Header;
